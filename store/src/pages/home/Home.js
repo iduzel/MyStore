af6a96fe91c479b0ad4acb4cae6a97ebe569784f
ismail <iduzel49@gmail.com>
2022-03-23 15:10:24 +0100

    logout function added. needed fix.

--- store/src/pages/home/Home.js
+++ store/src/pages/home/Home.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../context/Context'
 import './Home.scss'
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  const {userData, setUserData} = useContext(DataContext)
+  console.log('dataContext is:', userData)
+
+  const handleLogout = () => {
+    // clear the context
+    setUserData(null);
+    // redirect user to home
+    
+    navigate("/login");
+    console.log('after logout userDataContext is:', userData)
+    
+    
+  };
+ 
   return (
     <div className='home'>
       <h1>Home Home Sweet Home</h1>
+      <button onClick={handleLogout}>LOGOUT</button>
     </div>
   )
 }
