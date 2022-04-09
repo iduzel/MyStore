faaa83fcc4a24cfae78120fcfdbdcda78f472af0
ismail <iduzel49@gmail.com>
2022-04-09 02:28:05 +0200

    Add/Edit employee added

---
+++ store/src/components/employee/EditEmployee.js
@@ -0,0 +1,127 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { DataContext } from "../../pages/context/Context";
+import "./Employee.scss";
+import { useNavigate } from "react-router";
+import axios from "axios";
+
+const EditEmployee = ({ flag, setFlag, theEmployee }) => {
+  const navigate = useNavigate();
+  const employee = theEmployee;
+
+  const { updateEmployee, employeeData, setEmployeeData, userData } =
+    useContext(DataContext);
+
+  //handle edit
+  const [editData, setEditData] = useState({
+    name: employee.name,
+    email: employee.email,
+    address: employee.address,
+    phone: employee.phone,
+    department: employee.department,
+    date: employee.date,
+  });
+  console.log("editData is: ", editData);
+
+  const onInputChange = (e) => {
+    setEditData({ ...editData, [e.target.name]: e.target.value });
+  };
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+
+  //MODAL
+  const handleSubmitModal = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.put(
+      `/employees/edit/${employee._id}`,
+      editData
+    );
+
+    if (response.data.success) {
+      console.log("edit resp is : success");
+      const temp = [...employeeData];
+      temp.filter((item) => item._id !== employee._id);
+      setEmployeeData([...temp, editData]);
+    }
+
+    setFlag(!flag);
+
+    handleClose();
+  };
+
+  return (
+    <Form onSubmit={handleSubmitModal}>
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="Name *"
+          name="name"
+          value={editData.name}
+          onChange={(e) => onInputChange(e)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control
+          type="email"
+          placeholder="Email *"
+          name="email"
+          value={editData.email}
+          onChange={(e) => onInputChange(e)}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control
+          as="textarea"
+          placeholder="Address *"
+          name="address"
+          rows={3}
+          value={editData.address}
+          onChange={(e) => onInputChange(e)}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="Phone"
+          name="phone"
+          value={editData.phone}
+          onChange={(e) => onInputChange(e)}
+        />
+      </Form.Group>
+
+        <Form.Group>
+        <Form.Select  name="department"
+          value={editData.department}
+          onChange={(e) => onInputChange(e)} aria-label="Default select example">
+          <option>Department</option>
+          <option value="JS">JS</option>
+          <option value="Java">Java</option>
+          <option value="React">React</option>
+        </Form.Select>
+        </Form.Group>
+     
+      <Form.Group>
+        <Form.Control
+          type="date"
+          placeholder="Date"
+          name="date"
+          value={editData.date}
+          onChange={(e) => onInputChange(e)}
+        />
+      </Form.Group>
+
+      <Button variant="success" type="submit" block="true">
+        Update Employee
+      </Button>
+    </Form>
+  );
+};
+
+export default EditEmployee;
--- store/src/components/employee/EmployeeList.js
+++ store/src/components/employee/EmployeeList.js
@@ -3,19 +3,26 @@ import "./Employee.scss";
 import { Alert, Button, Form, Modal } from "react-bootstrap";
 import { DataContext } from "../../pages/context/Context";
 import axios from "axios";
+import { useNavigate } from "react-router";
+import Employee from "./Employee";
+import AddEmployee from "./AddEmployee";
 
 const EmployeeList = () => {
-  const { employeeData, setEmployeeData } = useContext(DataContext);
-  const [modalIndex, setModalIndex] = useState();
+  const navigate = useNavigate();
+  const [flag, setFlag] = useState();
+
+  const { userData, setUserData, employeeData, setEmployeeData } =
+    useContext(DataContext);
 
   useEffect(() => {
+    if (!userData) navigate("/login");
     const getData = async () => {
       const response = await axios.get("/employees/list");
       console.log("DATA EMP RESPONSE: ", response);
       setEmployeeData([...response.data]);
     };
     getData();
-  }, []);
+  }, [flag]);
 
   const sortedEmployees = employeeData?.sort((a, b) =>
     a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1
@@ -23,10 +30,8 @@ const EmployeeList = () => {
 
   const [showAlert, setShowAlert] = useState(false);
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  //const handleShowAlert = () => setShowAlert(true)
 
   const handleShowAlert = () => {
     setShowAlert(true);
@@ -34,13 +39,6 @@ const EmployeeList = () => {
       setShowAlert(false);
     }, 2000);
   };
-
-  const [obj, setObj] = useState({
-    name: "",
-    email: "",
-    address: "",
-    phone: null,
-  });
 
   useEffect(() => {
     handleClose();
@@ -50,195 +48,90 @@ const EmployeeList = () => {
     };
   }, [employeeData]);
 
-  // handle change obj
-  const handleChange = (e) => {
-    setObj({ ...obj, [e.target.name]: e.target.value });
-  };
+   
+  const getSearch = (e) => {
+    const searchElement = e.target.value;
+    console.log('getSearch: ', e.target.value);
+   
 
-  // handle submit
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await axios.post("/employees/add", obj);
+ 
 
-    setObj({
-      name: "",
-      email: "",
-      address: "",
-      phone: null,
-    });
-  };
-
-  //handle delete
-  const handleDelete = (id) => {
-    let temp = [...employeeData];
-
-    const idx = temp?.findIndex((item, index) => index === id);
-
-    if (idx > -1) temp.splice(idx, 1);
-    setEmployeeData(temp);
-  };
-
-  //handle edit
-  const [editData, setEditData] = useState({});
-
-  const handleEdit = (id) => {
-    setShow(true);
-    let temp = [...employeeData];
-    const employee = temp?.find((item, index) => index === id);
-    setModalIndex(id);
-    console.log("employee : ", employee);
-    setEditData({
-      ...editData,
-      name: employee.name,
-      address: employee.address,
-      phone: employee.phone,
-    });
-
-    console.log("edit Data : ", editData);
-  };
-  //MODAL
-
-  const handleSubmitModal = (e) => {
-    e.preventDefault();
-    let temp = [...employeeData];
-    const idx = temp?.findIndex((item, index) => index === modalIndex);
-    console.log("idx: ", idx);
-    if (idx > -1) temp.splice(idx, 1);
-    setEmployeeData(temp);
-    setEmployeeData([...temp, editData]);
-    handleClose();
-  };
+    
+  }
 
   return (
-    <div className="employeeList container">
-      <div className="video-container mt-3">
-        <iframe
-          
+    <div className="employeeList container mt-2">
+      {/* <div className="video-container mt-3">
+        <iframe          
           src="https://www.youtube.com/embed/8C8ISYGD0ns"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-      </div>
+      </div> */}
 
-      <form onSubmit={handleSubmit} className="formGroup">
-        
-        <input
-          className="form-control w-50"
-          placeholder="name"
-          name="name"
-          onChange={handleChange}
-        />
-        <input
-          className="form-control w-50"
-          placeholder="email"
-          name="email"
-          onChange={handleChange}
-        />
-        <input
-          className="form-control w-50"
-          placeholder="address"
-          name="address"
-          onChange={handleChange}
-        />
-        <input
-          className="form-control w-50"
-          placeholder="phone"
-          name="phone"
-          onChange={handleChange}
-        />
-        <button type="submit" className="btn btn-danger">
-          ADD EMPLOYEE
-        </button>
-      </form>
-
-      <h1>OBJ</h1>
-      <div>
-        <h1>{obj.name}</h1>
-        <h1>{obj.address}</h1>
-        <h1>{obj.phone}</h1>
-      </div>
-
-      <h1>ARRAY</h1>
       <Alert show={showAlert} variant="success">
         Employee List successfully updated!.
       </Alert>
-      {sortedEmployees?.map((item, index) => {
-        return (
-          <div key={index} className="border border-danger w-50 m-2 p-2">
-            <ul className="">
-              <li>{item.name}</li>
-              <li>{item.address}</li>
-              <li>{item.phone}</li>
-            </ul>
-            <div className="d-flex justify-content-end ">
-              <button
-                onClick={() => handleEdit(index)}
-                className="btn btn-warning me-2"
-              >
-                edit
-              </button>
-              <button
-                onClick={() => handleDelete(index)}
-                className="btn btn-danger "
-              >
-                delete
-              </button>
-            </div>
-          </div>
-        );
-      })}
 
-      <h1>MODAL</h1>
+      <div className="table-title mt-5">
+        <div className="row">
+          <div className="col-sm-4">
+            <h2>
+              Manage <b>Employees</b>
+            </h2>
+          </div>
+          <div className="col-sm-4 ">
+            <input
+            className="ps-1"
+            placeholder="Search"            
+            onChange={(e) => getSearch(e)}  />
+          </div>
+          <div className="col-sm-4">
+            <Button
+              onClick={handleShow}
+              className="btn btn-success text-white"
+              data-toggle="modal"
+            >
+              <i className="material-icons">&#xE147;</i>{" "}
+              <span>Add New Employee</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Department</th>
+            <th>Date</th>
+           {/*  <th>ID</th> */}
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedEmployees?.map((employee, index) => (
+            <tr key={index}>
+              <Employee employee={employee}
+                        flag={flag}
+                        setFlag={setFlag} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <Modal show={show} onHide={handleClose}>
         <Modal.Header className="modal-header" closeButton>
-          <Modal.Title>Update Employee</Modal.Title>
+          <Modal.Title>Add Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={(e) => handleSubmitModal(e)}>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                placeholder="Name *"
-                name="name"
-                value={editData.name}
-                onChange={(e) =>
-                  setEditData({ ...editData, name: e.target.value })
-                }
-                required
-              />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Control
-                as="textarea"
-                placeholder="Address *"
-                name="address"
-                rows={3}
-                value={editData.address}
-                onChange={(e) =>
-                  setEditData({ ...editData, address: e.target.value })
-                }
-              />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Control
-                type="text"
-                placeholder="Phone"
-                name="phone"
-                value={editData.phone}
-                onChange={(e) =>
-                  setEditData({ ...editData, phone: e.target.value })
-                }
-              />
-            </Form.Group>
-
-            <Button variant="success" type="submit" block>
-              Update Employee
-            </Button>
-          </Form>
+          <AddEmployee flag={flag}
+          setFlag={setFlag} />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -246,6 +139,7 @@ const EmployeeList = () => {
           </Button>
         </Modal.Footer>
       </Modal>
+
     </div>
   );
 };
