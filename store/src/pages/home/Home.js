cc1cc0570ef387e66aa821e57448d639db5911cb
ismail <iduzel49@gmail.com>
2022-04-11 02:23:31 +0200

    bugs fixed

--- store/src/pages/home/Home.js
+++ store/src/pages/home/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Home.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Home Home Sweet Home</h1>
+      <Link to="/employee">EMPLOYEES</Link>
+       <div className="video-container m-3">
+        <iframe          
+          src="https://www.youtube.com/embed/8C8ISYGD0ns"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };
