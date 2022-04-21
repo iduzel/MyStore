00534e834abf201bc6f9d23f925b442bbb77b7cc
ismail <iduzel49@gmail.com>
2022-04-21 13:19:33 +0200

    updated

--- store/src/pages/context/Context.js
+++ store/src/pages/context/Context.js
@@ -1,24 +1,42 @@
 import axios from "axios";
-import { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react";
 
+export const DataContext = createContext();
 
+export default function DataProvider({ children }) {
+  const [flag, setFlag] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [employeeData, setEmployeeData] = useState([]);
+  const [categoryData, setCategoryData] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [singlePost, setSinglePost] = useState({});
 
-export const DataContext = createContext()
+  /*   const updateEmployee = (id, updatedEmployee) => {
+    setEmployeeData(
+      employeeData?.map((employee) =>
+        employee._id === id ? updatedEmployee : employee
+      )
+    );
+  }; */
 
-
-export default function DataProvider({children}) {
-
- 
-    const [flag, setFlag] = useState(false);
-    const [userData, setUserData] = useState(null)
-    const [employeeData, setEmployeeData] = useState([])
-
-    const updateEmployee = (id, updatedEmployee) => {
-        setEmployeeData(employeeData?.map((employee) => (employee._id === id ? updatedEmployee : employee)))
-    } 
-    
-
-    return <DataContext.Provider value={{flag, setFlag, userData, setUserData, employeeData, setEmployeeData, updateEmployee}}> 
-        {children}
+  return (
+    <DataContext.Provider
+      value={{
+        flag,
+        setFlag,
+        userData,
+        setUserData,
+        employeeData,
+        setEmployeeData,
+        categoryData,
+        setCategoryData,
+        posts,
+        setPosts,
+        singlePost,
+        setSinglePost,
+      }}
+    >
+      {children}
     </DataContext.Provider>
+  );
 }
