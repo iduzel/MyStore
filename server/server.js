a257579f6057a1030d9adaee8127392c3f15b31a
ismail <iduzel49@gmail.com>
2022-07-05 14:18:37 +0200

    fc9

--- server/server.js
+++ server/server.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const cookieParser = require('cookie-parser')
-const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors())
-
-
-
-
+app.use(cors());
 
 app.use("/users", require("./controllers/userController"));
 app.use("/employees", require("./controllers/employeeController"));
 app.use("/admin", require("./controllers/adminController"));
 app.use("/posts", require("./controllers/postController"));
 
-app.get("/", (req, res) => {
-  res.send("HELLO FROM STORE SERVIS");
-});
 const connectToMongoDB = require("./config/db");
 connectToMongoDB();
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server is running at port ${port}`));
+
+app.use(express.static("client/build"));
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+
+  app.get("/*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+  );
+}
+
